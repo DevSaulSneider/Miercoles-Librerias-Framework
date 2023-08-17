80b516d41aedba73c98e41dbf44823925c85a764
saulsneider7 <saulsneider@hotmail.com>
2023-08-16 20:13:33 -0500

    stylos nav

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Mi_Titulo } from './componentes/PrimerComponente';
 import { Mi_componente } from './componentes/SegundoComponente';
+import Navbar from './componentes/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar props={{homeUrl: '#', logoSrc: require("./assets/img/logo.png"), tituloWeb:"Mi Pagina en REACT", loginUrl:"#"}}/>
       <h1>Crack The Code</h1>
       <Mi_Titulo />
       <Mi_componente />
--- proyecto/src/componentes/Navbar.jsx
+++ proyecto/src/componentes/Navbar.jsx
@@ -1,3 +1,4 @@
+import '../assets/css/navbar.css';
 function Logo({props}) {
   return (
     <div className="navbar-logo">
@@ -23,14 +24,36 @@ function Secciones({props}) {
 
   for (let i = 0; i < numeroItems -1; i++) {
     unionItems.push(<Item props={{href : items[i].href, name: items[i].name}} />);
+    unionItems.push(<span className='navbar-secciones-barra'>|</span>);
   }
   unionItems.push(<Item props={{href: items[numeroItems - 1].href, name: items[numeroItems -1].name}} />);
-  return <div className="navbar-secciones"> {numeroItems}</div>;
+  return <div className="navbar-secciones"> {unionItems}</div>;
 }
 
 const items = [
-  {href: "#", name: "Home"},
+  {href: "#", name: "Inicio"},
   {href: "#", name: "Sobre Nosotros"},
   {href: "#", name: "Productos"},
   {href: "#", name: "Contacto"},
 ];
+
+function User ({props, auth}){
+  if({auth}){
+    // Se muetsra la foto de perfil del usuario
+  }else{
+    return <div>
+      <a href={props.url}>
+        <img src={require("../assets/img/no-auth-icon.png")} alt="Login"></img>
+      </a>
+    </div>
+  }
+}
+
+export default function Navbar({props}) {
+  const auth = false;
+  return <div className="navbar">
+    <Logo props={{href: props.homeUrl, src: props.logoSrc, alt: props.tituloWeb}} />
+    <Secciones props={{items: items}} />
+    <User props={{url: props.loginUrl}} auth={auth} />
+  </div>
+}
